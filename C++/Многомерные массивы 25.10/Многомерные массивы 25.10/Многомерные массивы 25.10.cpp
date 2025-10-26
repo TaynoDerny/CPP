17426cf38e9e952dedc707d0fe589b6738450b81
TaynoDerny <kirillbalabin7@gmail.com>
2025-10-26 23:03:24 +0900

    endl

---
+++ C++/Многомерные массивы 25.10/Многомерные массивы 25.10/Многомерные массивы 25.10.cpp
@@ -0,0 +1,85 @@
+﻿
+#include <iostream>
+#include <ctime>
+
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	
+    const int row = 2;
+    const int col = 4;
+	int arr[row][col];
+	int sum_arr = 0;
+	int min_element = NULL;
+	int max_element = NULL;
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			arr[i][j] = rand() % 10;
+			cout << arr[i][j] << " ";
+			sum_arr += arr[i][j];
+			if (arr[i][j] < min_element)
+				min_element = arr[i][j];
+			if (arr[i][j] > max_element)
+				max_element = arr[i][j];
+		}
+		cout << endl;
+	}
+	cout << "Сумму всех элементов массива = " <<sum_arr<< endl;
+	cout << "Среднее арифметическое всех элементов массива = " << sum_arr/(row+ col) << endl;
+	cout << "Минимальный элемент = " << min_element << endl;
+	cout << "Максимальный элемент = " << max_element << endl;
+	//Задача 2
+	for (int i = 0; i < row; i++)
+	{
+		int sum_arr = 0;
+		for (int j = 0; j < col; j++)
+		{
+			arr[i][j] = rand() % 10;
+			cout << arr[i][j] << " ";
+			sum_arr += arr[i][j];
+			if (j == col - 1)
+				cout << " | " << sum_arr;
+		}
+		cout << endl;
+	}
+	
+	
+	//Задача 3
+	const int row_1 = 5;
+	const int col_1 = 10;
+	int arr_1[row_1][col_1];
+
+	const int row_2 = 5;
+	const int col_2 = 5;
+	int arr_2[row_2][col_2];
+	
+	for (int i = 0; i < row_1; i++)
+	{
+		for (int j = 0; j < col_1; j++)
+		{
+			arr_1[i][j] = rand() % 50;
+			cout << arr_1[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	
+	for (int i = 0; i < row_2; i++)
+	{
+		
+		for (int j = 0; j < col_2; j++)
+		{
+			arr_2[i][j] = arr_1[i][j*2] + arr_1[i][j*2+1];
+			cout << arr_2[i][j] << " ";
+			
+		}   
+
+		cout << endl;
+	}
+}	
+
---
+++ C++/многомерные массивы 31.10/многомерные массивы 31.10/многомерные массивы 31.10.cpp
@@ -0,0 +1,103 @@
+﻿
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    srand(time(NULL));
+    const int row = 3;
+    const int col = 6;
+    int arr_1[row][col];
+    int num_1;
+    cout << "Введите число: " << endl;
+    cin >> num_1;
+    cout << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++ )
+        {
+            arr_1[i][j] = num_1;
+            cout << arr_1[i][j] << " ";
+            num_1 = num_1 * 2;
+        }
+        cout << endl;
+    }
+    //Задача  2
+    int arr_2[row][col];
+    int num_2;
+    cout << "Введите число: " << endl;
+    cin >> num_2;
+    cout << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for ( int j = 0; j < col; j++)
+        {
+            arr_2[i][j] = num_2;
+            cout << arr_2[i][j] << " ";
+            num_2++;
+        }
+        cout << endl;
+    }
+    
+   
+    //Задача 3
+    int arr_3[row][col];
+    int shift;
+    int sv;
+    cout << "Введите сдвиг: " << endl;
+    cin >> shift;
+    cout << "Введите положения сдвига 1 - влево; 2 - вправо; 3 - вверх; 4 - вниз; : " << endl;
+    cin >> sv;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            arr_3[i][j] = rand() % 9;
+            cout << arr_3[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            switch(sv){
+            case 1:
+                if (j + shift >= col)
+                    cout << arr_3[i][j + shift - col] << " ";
+                else
+                    cout << arr_3[i][j + shift] << " ";
+                break;
+                
+            case 2:
+                if (j - shift < 0)
+                    cout << arr_3[i][j - shift + col] << " ";
+                else
+                    cout << arr_3[i][j - shift] << " ";
+                break;
+            case 3:
+                if (i + shift >= row)
+                    cout << arr_3[i + shift - row][j] << " ";
+                else
+                    cout << arr_3[i + shift][j] << " ";
+                break;
+            case 4:
+                if (i - shift < 0)
+                    cout << arr_3[i - shift + row][j] << " ";
+                else
+                    cout << arr_3[i - shift][j] << " ";
+                break;
+
+            default:
+                return 0;
+
+        }
+            
+        }
+        cout << endl;
+    }
+
+}
+
